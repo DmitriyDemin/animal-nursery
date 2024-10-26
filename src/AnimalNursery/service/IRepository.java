1e696e27ae3ada82260be1b0059a014d99d9a163
Dmitriy_Demin <d.demin@inbox.ru>
2024-10-26 10:05:34 +0400

    подключил локальную БД

--- src/AnimalNursery/service/IRepository.java
+++ src/AnimalNursery/service/IRepository.java
@@ -1,10 +1,14 @@
 package AnimalNursery.service;
 
-public interface IStorage <Animal> {
-    List <Animal> getAll();
-    Animal getById(int id);
+
+import java.util.List;
+
+public interface IRepository<Animal> {
+
+    List<Animal> getAll();
+    /*Animal getById(int id);
     int create (Animal item);
     int update(Animal item);
-    void delete (int item);
+    void delete (int item);*/
 
     }
--- src/AnimalNursery/service/Repository.java
+++ src/AnimalNursery/service/Repository.java
@@ -1,2 +1,84 @@
-package AnimalNursery.service;public class Repository {
-}
+package AnimalNursery.service;
+
+import AnimalNursery.model.Animal;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.*;
+import java.time.LocalDate;
+
+
+
+public class Repository implements IRepository <Animal> {
+
+    private String SQLquery;
+    private Statement sqlStr;
+    private ResultSet resultSet;
+    private List<Animal> nursery = new ArrayList<Animal>();
+
+    public Repository (){
+
+    }
+
+    @Override
+    public List<Animal> getAll() {
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection dbConnection = DriverManager.getConnection("jdbc:MySQL://localhost:3306/human_friends", "root", "Yar_1188")) {
+
+                sqlStr = dbConnection.createStatement();
+                SQLquery = "SELECT A.animal_id, A.nickname, A.birthday, B.name FROM animals_nursery AS A INNER JOIN animals_type AS B ON A.animal_type = B.id ORDER BY A.animal_id";
+                resultSet = sqlStr.executeQuery(SQLquery);
+                while (resultSet.next()) {
+
+                    int id = resultSet.getInt(1);
+                    String name = resultSet.getString(2);
+                    LocalDate birthday = resultSet.getDate(3).toLocalDate();
+                    String type_id = resultSet.getString(4);
+                    Animal animal = new Animal();
+                    animal.setAnimal_id(id);
+                    animal.setNickname(name);
+                    animal.setBirthday(birthday);
+                    animal.setType(type_id);
+
+                    nursery.add(animal);
+                }
+            }
+            return nursery;
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
+    }
+    }
+//
+//
+//
+//
+//
+//
+//
+//
+//    @Override
+//    public Animal getById(int id) {
+//        return null;
+//    }
+//
+//    @Override
+//    public int create(Animal item) {
+//        return 0;
+//    }
+//
+//    @Override
+//    public int update(Animal item) {
+//        return 0;
+//    }
+//
+//    @Override
+//    public void delete(int item) {
+//
+//    }
+//}
+
